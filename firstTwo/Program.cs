6c8da6e105c535d24c11a9bd2a56becea69f73d5
«Vitaly» <statskvo@mail.ru>
2022-08-12 23:20:28 +0300

    add folder secondTwo

--- firstTwo/Program.cs
+++ firstTwo/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+int a1 = 20, b1 = 25, c1 = 30,
+    a2 = 30, b2 = 35, c2 = 40,
+    a3 = 40, b3 = 50, c3 = 45;
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+int max = Max(max1, max2, max3);
+
+/* int max = a1;
+if(b1 > max) max = b1;
+if(c1 > max) max = c1;
+if(a2 > max) max = a2;
+if(b2 > max) max = b2;
+if(c2 > max) max = c2;
+if(a3 > max) max = a3;
+if(b3 > max) max = b3;
+if(c3 > max) max = c3; */
+
+Console.WriteLine(max);
